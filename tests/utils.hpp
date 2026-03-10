6d6321671f375157359526e6ecebd7c624b7cd0d
Andrzej Antoni Kwaśniewski <andrzej.kwasniewski@swmansion.com>
2026-03-10 13:54:29 +0100

    utility to check approximations

---
+++ tests/utils.hpp
@@ -0,0 +1,8 @@
+#include <cmath>
+
+bool approx_equal(double value, double expected, double error);
+
+bool approx_equal(double value, double expected, double error)
+{
+    return std::abs(value - expected) <= error*1.5;
+}
