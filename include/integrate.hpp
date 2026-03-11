24fd545c241a0bab50f21f98971e18cff69f033b
a-szymanska <szymanska.mariaanna@gmail.com>
2026-03-11 08:25:44 +0100

    Refactoring

--- include/integrate.hpp
+++ include/integrate.hpp
@@ -69,53 +69,51 @@ Result integrate_MC(
     std::mt19937 mt(time(nullptr));
     std::vector<box> bins(n_boxes);
 
-    double bin_size=(upper-lower)/n_boxes;
-    double bin_size_sq=bin_size*bin_size;
+    double bin_size = (upper-lower) / n_boxes;
 
     std::uniform_real_distribution<double> dist(0, bin_size);
     double cur = lower;
 
-    for(int i=0; i<n_boxes; i++){
-        bins[i]={n_points/n_boxes, cur};
-        cur+=bin_size;
+    for (int i = 0; i < n_boxes; i++) {
+        bins[i] = {n_points / n_boxes, cur};
+        cur += bin_size;
     }
 
     double err_sum = 0;
     double result_sum = 0;
 
-    for(int i=0; i<n_iterations; i++){
+    for (int i = 0; i < n_iterations; i++) {
         double int_sum = 0;
 
-        for(auto& bin: bins){
+        for (auto& bin: bins) {
             double sum = 0;
-            double mean=0;
-            double m2=0;
+            double mean = 0;
+            double m2 = 0;
             
-            for(int k=1; k<=bin.points; k++){
-                double x = dist(mt)+ bin.l;
+            for (int k = 1; k <= bin.points; k++) {
+                double x = dist(mt) + bin.l;
                 double y = f(x);
-                sum+=y;
+                sum += y;
 
-                //welford's variance
+                // Welford's variance
                 double old_mean = mean;
-                mean+=(y-mean)/k;
-                m2+=(y-old_mean)*(y-mean);
+                mean += (y-mean) / k;
+                m2 += (y-old_mean) * (y-mean);
             }
-            bin.cur_integral = bin_size*sum/bin.points;
-            int_sum+=bin.cur_integral;
+            bin.cur_integral = bin_size * sum / bin.points;
+            int_sum += bin.cur_integral;
             
-            err_sum += bin_size_sq*m2/(bin.points*(bin.points-1));
+            err_sum += bin_size * bin_size * m2 / (bin.points * (bin.points-1));
 
         }
 
-        for(auto& bin: bins){
-            double contribution = fabs(bin.cur_integral/int_sum);
-            bin.points=int(n_points*contribution);
-            if(bin.points<2) bin.points=2;
+        for (auto& bin: bins) {
+            double contribution = fabs(bin.cur_integral / int_sum);
+            bin.points = std::max(2, int(n_points * contribution)); // ensure at least 2 points per bin
         }
-        result_sum+=int_sum;
+        result_sum += int_sum;
     }
-    return {result_sum/n_iterations, sqrt(err_sum)/n_iterations};
+    return {result_sum / n_iterations, sqrt(err_sum) / n_iterations};
 }
 
 Result integrate_MC_ndim(
